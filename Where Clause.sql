087c42c0e2a1d6bcef07f820fbdd4ef92593a2ba
MorsyLab <110126117+MorsyLab@users.noreply.github.com>
2025-10-05 20:11:19 +0300

    Where Clause

---
+++ Where Clause.sql
@@ -0,0 +1,19 @@
+-- Select all rows where gender is NOT 'Male'
+SELECT *
+FROM your_schema.your_table
+WHERE gender LIKE '%' AND NOT gender = 'Male';
+
+-- Same as above but simplified
+SELECT *
+FROM your_schema.your_table
+WHERE NOT gender = 'Male';
+
+-- Select rows where first_name starts with 'a' and is exactly 4 characters long
+SELECT *
+FROM your_schema.your_table
+WHERE first_name LIKE 'a___';
+
+-- Select rows where first_name is exactly 4 characters long
+SELECT *
+FROM your_schema.your_table
+WHERE first_name LIKE '____';
